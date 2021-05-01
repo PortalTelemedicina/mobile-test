2eabeb08932e8e1f2262c3bf45a917bb9edbc86b
Victor Takai <kalivos.takai@gmail.com>
2021-04-30 21:12:04 -0300

    Created iOS project

---
+++ MobileTest/Shared/MobileTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTestApp.swift
+//  Shared
+//
+//  Created by Victor H. Rezende Takai on 30/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MobileTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
