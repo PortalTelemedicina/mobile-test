7fe967346b35c567fdab0c6631c136874135ed51
Vofchuk <daniel.vofchuk@gmail.com>
2021-05-23 17:03:56 -0300

    Final configurations

--- lib/api/specialists_api.dart
+++ lib/api/specialists_api.dart
@@ -8,10 +8,16 @@ import 'api.dart';
 ///Class for handle the specislist repository wiht all calls needed for this specific API
 class SpecialistApi {
   static Future<List<Specialist>> getSpecialists() async {
-    Response res = await Api.call('list_specialist_heart');
-    return List.from(jsonDecode(res.body))
-        .map((e) => Specialist.fromMap(
-            e)) //Creates the local obj with the data fetched form the API
-        .toList(); //Returs the list
+    try {
+      Response res = await Api.call('list_specialist_heart');
+      print(res);
+      return List.from(jsonDecode(res.body))
+          .map((e) => Specialist.fromMap(
+              e)) //Creates the local obj with the data fetched form the API
+          .toList(); //Returs the list
+    } on Exception catch (e) {
+      print(e);
+      throw e;
+    }
   }
 }
