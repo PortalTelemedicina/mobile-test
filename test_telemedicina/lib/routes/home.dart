6b6ab22d386e6c53f4c7e45aaa07252a92b7ce60
Julio <julio.c.filho@hotmail.com>
2021-09-26 00:55:45 -0300

    bottom bar adjust (to be completed) and icons and fonts

--- test_telemedicina/lib/routes/home.dart
+++ test_telemedicina/lib/routes/home.dart
@@ -1,14 +1,26 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:test_telemedicina/repository/datasource/specialists.dart';
 import 'package:test_telemedicina/repository/entities/specialist.dart';
 
-class Home extends StatelessWidget {
-  final RxInt _currentIndex = 0.obs;
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
+class _HomeState extends State<Home> with TickerProviderStateMixin {
   final RxList<Specialist> _specialists =
       Get.find<SpecialistsDatasource>().specialists;
+
+  late final Rx<PageController> _controller;
+
+  @override
+  void initState() {
+    _controller = PageController().obs;
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,76 +32,143 @@ class Home extends StatelessWidget {
       ),
       home: SafeArea(
         child: Scaffold(
-          bottomNavigationBar: Obx(
-            () => BottomNavigationBar(
-              currentIndex: _currentIndex.value,
-              fixedColor: Colors.black54,
-              onTap: (int i) => _currentIndex.value = i,
-              items: [
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.file_copy,
-                    color: Colors.black45,
-                  ),
-                  label: 'Home',
-                ),
-                BottomNavigationBarItem(
-                    icon: Icon(
-                      Icons.message,
-                      color: Colors.black45,
-                    ),
-                    label: 'Messages'),
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.autorenew_rounded,
-                    color: Colors.black45,
-                  ),
-                  label: 'Quick',
-                ),
-                BottomNavigationBarItem(
-                  icon: Icon(
-                    Icons.menu,
-                    color: Colors.black45,
-                  ),
-                  label: 'Menu',
-                ),
-              ],
-            ),
-          ),
-          body: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Container(
-              color: Colors.white,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
+          backgroundColor: Colors.white,
+          persistentFooterButtons: [
+            Padding(
+              padding: const EdgeInsets.only(left: 18, right: 18),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text(
-                    'Hello,',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 16,
-                    ),
+                  _bottomNavButton(
+                    'assets/icons/diagnostic.svg',
+                    index: 0,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8),
-                    child: Text(
-                      'Lorelle Luna',
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.w800,
-                      ),
-                    ),
+                  _bottomNavButton(
+                    'assets/icons/diagnostic.svg',
+                    index: 1,
                   ),
-                  _label('Specialists'),
-                  _doctorsList(),
-                  _label('What do you need?'),
-                  _quickAccess(),
+                  _bottomNavButton(
+                    'assets/icons/diagnostic.svg',
+                    index: 2,
+                  ),
+                  _bottomNavButton(
+                    'assets/icons/diagnostic.svg',
+                    index: 3,
+                  ),
                 ],
               ),
             ),
+          ],
+          body: Stack(
+            children: [
+              PageView(
+                pageSnapping: true,
+                controller: _controller.value,
+                children: [
+                  _homeTab(),
+                  _toBeImplementedTab(),
+                  _toBeImplementedTab(),
+                  _toBeImplementedTab(),
+                ],
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _navButtonIndicator(
+                        activated: _controller.value.page == 0,
+                      ),
+                      _navButtonIndicator(
+                        activated: _controller.value.page == 1,
+                      ),
+                      _navButtonIndicator(
+                        activated: _controller.value.page == 2,
+                      ),
+                      _navButtonIndicator(
+                        activated: _controller.value.page == 3,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
+      ),
+    );
+  }
+
+  Container _navButtonIndicator({bool activated = false}) {
+    return Container(
+      height: 3,
+      width: 30,
+      color: activated ? Colors.black : Colors.black54,
+    );
+  }
+
+  InkWell _bottomNavButton(
+    String assetPath, {
+    int index = 0,
+  }) {
+    return InkWell(
+      child: Container(
+        height: 30,
+        child: _navIcon(assetPath),
+      ),
+      onTap: () => _controller.value.jumpToPage(index),
+    );
+  }
+
+  Widget _navIcon(
+    String svgName, {
+    Color color = Colors.black38,
+    bool activated = false,
+  }) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        SvgPicture.asset(
+          svgName,
+          color: activated ? Colors.black54 : color,
+          height: 20,
+          width: 20,
+        ),
+      ],
+    );
+  }
+
+  Widget _homeTab() {
+    return Container(
+      color: Colors.white,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            'Hello,',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              fontSize: 18,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8),
+            child: Text(
+              'Lorelle Luna',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+          ),
+          _label('Specialists'),
+          _doctorsList(),
+          _label('What do you need?'),
+          _quickAccess(),
+        ],
       ),
     );
   }
@@ -100,7 +179,7 @@ class Home extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _button(
+            _quickButton(
               SvgPicture.asset(
                 'assets/icons/diagnostic.svg',
                 height: 30,
@@ -110,7 +189,7 @@ class Home extends StatelessWidget {
               'Diagnostic',
               activated: true,
             ),
-            _button(
+            _quickButton(
               SvgPicture.asset(
                 'assets/icons/consultation.svg',
                 height: 30,
@@ -118,7 +197,7 @@ class Home extends StatelessWidget {
               ),
               'Consultation',
             ),
-            _button(
+            _quickButton(
               SvgPicture.asset(
                 'assets/icons/nurse.svg',
                 height: 30,
@@ -131,7 +210,7 @@ class Home extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            _button(
+            _quickButton(
               SvgPicture.asset(
                 'assets/icons/ambulance.svg',
                 height: 40,
@@ -139,7 +218,7 @@ class Home extends StatelessWidget {
               ),
               'Ambulance',
             ),
-            _button(
+            _quickButton(
               SvgPicture.asset(
                 'assets/icons/labwork.svg',
                 height: 30,
@@ -147,7 +226,7 @@ class Home extends StatelessWidget {
               ),
               'Lab Work',
             ),
-            _button(
+            _quickButton(
               SvgPicture.asset(
                 'assets/icons/medicine.svg',
                 height: 30,
@@ -161,7 +240,7 @@ class Home extends StatelessWidget {
     );
   }
 
-  Padding _button(
+  Widget _quickButton(
     Widget svg,
     String title, {
     bool activated = false,
@@ -172,31 +251,37 @@ class Home extends StatelessWidget {
     Color grey = Colors.black12;
     grey = grey.withAlpha(20);
 
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 100,
-        width: 100,
-        decoration: BoxDecoration(
-          color: activated ? black : grey,
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Flexible(child: svg),
-            Padding(
-              padding: const EdgeInsets.only(top: 8),
-              child: Text(
-                title,
-                style: TextStyle(
-                  color: activated ? Colors.white : Colors.black54,
-                  fontWeight: FontWeight.w600,
-                  fontSize: 12,
-                ),
-              ),
+    return Flexible(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: InkWell(
+          onTap: () {},
+          splashColor: Colors.grey,
+          child: Container(
+            height: 105,
+            width: 105,
+            decoration: BoxDecoration(
+              color: activated ? black : grey,
+              borderRadius: BorderRadius.circular(12),
             ),
-          ],
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(child: svg),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: Text(
+                    title,
+                    style: TextStyle(
+                      color: activated ? Colors.white : Colors.black54,
+                      fontWeight: FontWeight.w600,
+                      fontSize: 12,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -270,8 +355,23 @@ class Home extends StatelessWidget {
       child: Text(
         _label,
         style: TextStyle(
-          fontSize: 16,
+          fontSize: 18,
           fontWeight: FontWeight.w600,
+        ),
+      ),
+    );
+  }
+
+  Widget _toBeImplementedTab() {
+    return Container(
+      color: Colors.grey,
+      child: Center(
+        child: Text(
+          'To be\nimplemented...',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 38,
+          ),
         ),
       ),
     );
