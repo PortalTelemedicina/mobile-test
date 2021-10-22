48b1186a3e6ebae992a143b3f2e81a7c19a566e9
Vitor Veras <vitorverasm@gmail.com>
2021-10-22 09:34:21 -0300

    chore: update jest setup and add lib @testing-library/react-native

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+  },
+  env: {
+    'jest/globals': true,
+  },
 };
---
+++ jest.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  preset: 'react-native',
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/config/**/*',
+    '!<rootDir>/src/presentation/routes/**/*',
+    '!**/*.d.ts',
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.(js|ts|tsx)$': 'babel-jest',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?@react-native|react-native|react-navigation|@react-native-community|@ui-kitten|native-base-shoutem-theme|@react-navigation)',
+  ],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.svg': '<rootDir>/__mocks__/svgMock.js',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+};
