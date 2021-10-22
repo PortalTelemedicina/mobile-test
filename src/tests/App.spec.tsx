19f95dd0996e83e4eb6e0b493c4b1a43ea9b47a7
Vitor Veras <vitorverasm@gmail.com>
2021-10-22 09:38:44 -0300

    test: setup test with @testing-library/react-native

--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -1,14 +1,9 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
+import 'react-native';
 import App from '../App';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const {getByText} = render(<App />);
+  expect(getByText('Home')).toBeDefined();
 });
