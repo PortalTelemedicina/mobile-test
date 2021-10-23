402ea387db716377c0e7d23ef6f93041522a8cbb
Vitor Veras <vitorverasm@gmail.com>
2021-10-23 18:54:43 -0300

    feat: add Router to main entry point

--- src/app/main/index.tsx
+++ src/app/main/index.tsx
@@ -1,53 +1,18 @@
 import {UIKittenProvider} from '@/app/UI/shared/components/ui-kitten-provider/ui-kitten-provider';
-import {Button, Icon, Layout, Text} from '@ui-kitten/components';
-import React, {useState} from 'react';
-import {SafeAreaView, StyleProp, StyleSheet, ViewStyle} from 'react-native';
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Router} from '@/app/UI/shared/routes';
+import {NavigationContainer} from '@react-navigation/native';
+import React from 'react';
 
 const App = () => {
-  const [count, setCount] = useState(0);
   const isDarkMode = false;
 
-  const backgroundStyle: StyleProp<ViewStyle> = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-    flex: 1,
-  };
-
   return (
-    <>
+    <NavigationContainer>
       <UIKittenProvider darkMode={isDarkMode}>
-        <SafeAreaView style={backgroundStyle}>
-          <Layout style={styles.container}>
-            <Text category="h1" testID="title">
-              Home
-            </Text>
-            <Text style={styles.couter} category="s1" testID="count">
-              {count}
-            </Text>
-            <Button
-              style={styles.button}
-              onPress={() => setCount(prevCount => prevCount + 1)}
-              accessoryLeft={props => (
-                <Icon name="plus-square-outline" {...props} />
-              )}
-            >
-              Add
-            </Button>
-          </Layout>
-        </SafeAreaView>
+        <Router />
       </UIKittenProvider>
-    </>
+    </NavigationContainer>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  couter: {marginTop: 20},
-  button: {marginTop: 20},
-});
 
 export default App;
