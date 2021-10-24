e8c6c01c5eaf703f5ec34acef47cb911bb852403
Vitor Veras <vitorverasm@gmail.com>
2021-10-24 16:18:11 -0300

    feat: add RouteList

--- src/app/UI/shared/routes/index.ts
+++ src/app/UI/shared/routes/index.ts
@@ -1,1 +1,20 @@
+export {default as HomeRoutes} from './home/home.routes';
 export {default as Router} from './router';
+
+// Route List
+export type AvailableRoutes =
+  | 'LOGIN'
+  | 'HOME'
+  | 'APPOINTMENT'
+  | 'CHAT'
+  | 'ACTIVITY'
+  | 'MENU';
+
+export type RouteList = {
+  LOGIN: undefined;
+  HOME: undefined;
+  APPOINTMENT: undefined;
+  CHAT: undefined;
+  ACTIVITY: undefined;
+  MENU: undefined;
+};
