a2927f59d39d259d43c14aea895ff5b0e877a022
Vitor Veras <vitorverasm@gmail.com>
2021-10-24 16:17:45 -0300

    feat: export components and pages

--- src/app/UI/pages/index.ts
+++ src/app/UI/pages/index.ts
@@ -1,2 +1,5 @@
-export {default as Home} from './home/home';
+export {default as Activity} from './activity/activity';
+export {default as Chat} from './chat/chat';
+export {default as ListSpecialists} from './list-specialists/list-specialists';
 export {default as Login} from './login/login';
+export {default as Menu} from './menu/menu';
---
+++ src/app/UI/shared/components/index.ts
@@ -0,0 +1,4 @@
+export {HomeBottomBar} from './home-bottom-bar/home-bottom-bar';
+export {UIKittenProvider} from './ui-kitten-provider/ui-kitten-provider';
+export {SafeAreaContainer} from './safe-area-container/safe-area-container';
+export {HomeHeader, HomeHeaderProps} from './home-header/home-header';
