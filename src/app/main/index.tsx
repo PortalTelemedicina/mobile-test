710f38cd3739a544736f2362aaa555ff2b819274
Vitor Veras <vitorverasm@gmail.com>
2021-10-24 16:18:47 -0300

    refactor: add SafeAreaProvider and a view to capture entry point on tests

--- src/app/main/index.tsx
+++ src/app/main/index.tsx
@@ -1,18 +1,25 @@
-import {UIKittenProvider} from '@/app/UI/shared/components/ui-kitten-provider/ui-kitten-provider';
+import {UIKittenProvider} from '@/app/UI/shared/components';
 import {Router} from '@/app/UI/shared/routes';
+import {GlobalStyles} from '@/app/UI/shared/styles/global-styles';
 import '@/config/locale';
 import {NavigationContainer} from '@react-navigation/native';
 import React from 'react';
+import {View} from 'react-native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const App = () => {
   const isDarkMode = false;
 
   return (
-    <NavigationContainer>
-      <UIKittenProvider darkMode={isDarkMode}>
-        <Router />
-      </UIKittenProvider>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <UIKittenProvider darkMode={isDarkMode}>
+          <View style={GlobalStyles.container} testID="app-entrypoint">
+            <Router />
+          </View>
+        </UIKittenProvider>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
