ec97076e15dd99236ccfdb3c7359311207f697ae
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 16:33:55 -0300

    test: add mockMedicalSpecialty function

--- tests/mocks/helpers/mock-medical-specialties.ts
+++ tests/mocks/helpers/mock-medical-specialties.ts
@@ -1,16 +1,20 @@
 import {MedicalSpecialties} from '@/app/domain/entities';
 import faker from 'faker';
 
+export const mockMedicalSpecialty = (): MedicalSpecialties => {
+  return {
+    name: faker.random.words(1),
+    image: faker.random.words(1),
+    amountAvailable: faker.datatype.number({min: 0, max: 20}),
+    color: faker.random.words(1),
+  };
+};
+
 export const mockMedicalSpecialties = (): MedicalSpecialties[] => {
   const response: MedicalSpecialties[] = [];
   const randomAmount = faker.datatype.number({min: 0, max: 5});
   for (let index = 0; index < randomAmount; index++) {
-    response.push({
-      name: faker.random.words(1),
-      image: faker.random.words(1),
-      amountAvailable: faker.datatype.number({min: 0, max: 20}),
-      color: faker.random.words(1),
-    });
+    response.push(mockMedicalSpecialty());
   }
   return response;
 };
