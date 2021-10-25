4ee1cd952b25ff05eb52a607734780b6d2bed2be
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 08:55:41 -0300

    test: ensure ListSpecialists call listQuickActions on initialization

--- tests/unit/app/UI/pages/list-specialists/list-specialists.spec.tsx
+++ tests/unit/app/UI/pages/list-specialists/list-specialists.spec.tsx
@@ -1,3 +1,4 @@
+import {ListQuickActionsSpy} from '@/../tests/mocks/spies/usecases/list-quick-actions-spy';
 import {ListSpecialists} from '@/app/UI/pages';
 import {ListSpecialistsProps} from '@/app/UI/pages/list-specialists/list-specialists';
 import {UIKittenProvider} from '@/app/UI/shared/components';
@@ -8,23 +9,30 @@ import 'react-native';
 
 type InitialState = {
   sut: RenderAPI;
+  listQuickActionsSpy: ListQuickActionsSpy;
 };
 
-const getInitialState = (params: ListSpecialistsProps): InitialState => {
+const getInitialState = (): InitialState => {
+  const listQuickActionsSpy = new ListQuickActionsSpy();
   const sut = render(
     <UIKittenProvider>
-      <ListSpecialists />
+      <ListSpecialists listQuickActions={listQuickActionsSpy} />
     </UIKittenProvider>,
   );
-  return {sut};
+  return {sut, listQuickActionsSpy};
 };
 
 describe('ListSpecialists', () => {
   afterEach(cleanup);
 
   test('should render with initial state', () => {
-    const {sut} = getInitialState({});
+    const {sut} = getInitialState();
     expect(sut.getByText('Specialists')).toBeDefined();
     expect(sut.getByText('What do you need?')).toBeDefined();
+  });
+
+  test('should call ListQuickActions on initial render', () => {
+    const {sut, listQuickActionsSpy} = getInitialState();
+    expect(listQuickActionsSpy.calls).toBe(1);
   });
 });
