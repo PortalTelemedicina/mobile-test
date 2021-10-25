8057949869a42f8b883e6ea77895ba73d642a43b
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 14:29:06 -0300

    feat: export props for page ListSpecialists

--- src/app/UI/pages/index.ts
+++ src/app/UI/pages/index.ts
@@ -1,5 +1,8 @@
 export {default as Activity} from './activity/activity';
 export {default as Chat} from './chat/chat';
-export {default as ListSpecialists} from './list-specialists/list-specialists';
+export {
+  default as ListSpecialists,
+  ListSpecialistsProps,
+} from './list-specialists/list-specialists';
 export {default as Login} from './login/login';
 export {default as Menu} from './menu/menu';
