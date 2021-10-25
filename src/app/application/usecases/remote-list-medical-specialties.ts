eb0650f81121ee4234065d0cb1a2c3a2edd6cfc4
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 14:52:08 -0300

    refactor: update RemoteListMedicalSpecialties to parse correctly the endpoint response

--- src/app/application/usecases/remote-list-medical-specialties.ts
+++ src/app/application/usecases/remote-list-medical-specialties.ts
@@ -1,19 +1,28 @@
 import {MedicalSpecialties} from '@/app/domain/entities';
 import {ConnectionError, ServerError} from '@/app/domain/errors';
 import {HttpGetClient, HttpStatusCode} from '@/app/domain/protocols/http';
+import {MedicalSpecialtiesResponse} from '@/app/domain/protocols/http/responses';
 import {ListMedicalSpecialties} from '@/app/domain/usecases';
 
 export class RemoteListMedicalSpecialties implements ListMedicalSpecialties {
   constructor(
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient<MedicalSpecialties[]>,
+    private readonly httpGetClient: HttpGetClient<MedicalSpecialtiesResponse[]>,
   ) {}
   async run(): Promise<MedicalSpecialties[]> {
     const httpResponse = await this.httpGetClient.get({url: this.url});
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        const medicalSpecialtiesArray = httpResponse.data;
+        const medicalSpecialtiesArray: MedicalSpecialties[] =
+          httpResponse.data.map(specialty => {
+            return {
+              name: specialty.name,
+              image: specialty.image_url,
+              color: specialty.color,
+              amountAvailable: specialty.total,
+            };
+          });
         return medicalSpecialtiesArray;
       case HttpStatusCode.connectionError:
         throw new ConnectionError();
