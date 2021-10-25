dcfad885f2004fabb1811c1260a8cc38f01b3f01
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 08:34:08 -0300

    feat: add section titles specialists and whatDoYouNeed

--- src/app/UI/pages/list-specialists/list-specialists.tsx
+++ src/app/UI/pages/list-specialists/list-specialists.tsx
@@ -1,17 +1,21 @@
+import {ListSpecialistsI18n, SharedI18n, translate} from '@/config/locale';
 import {Layout, Text} from '@ui-kitten/components';
 import React from 'react';
 import {StyleSheet} from 'react-native';
 
-const ListSpecialists = ({}) => {
+export type ListSpecialistsProps = {};
+
+const ListSpecialists: React.FC<ListSpecialistsProps> = ({}) => {
   return (
     <Layout level="1" style={styles.container}>
-      <Text category="h1">ListSpecialists</Text>
+      <Text category="h5">{translate(SharedI18n.specialists)}</Text>
+      <Text category="h5">{translate(ListSpecialistsI18n.whatDoYouNeed)}</Text>
     </Layout>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {flex: 1, justifyContent: 'center', alignItems: 'center'},
+  container: {flex: 1, paddingHorizontal: 20, paddingTop: 30},
 });
 
 export default ListSpecialists;
