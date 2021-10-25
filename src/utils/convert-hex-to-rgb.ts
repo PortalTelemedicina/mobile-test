1eae261d03095bb38a39dc5b44ccb9498a7e377a
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 16:55:16 -0300

    feat: ensure convertHexToRgb throws BadHexColorError if invalid hex is passed

--- src/utils/convert-hex-to-rgb.ts
+++ src/utils/convert-hex-to-rgb.ts
@@ -1,16 +1,22 @@
+import {BadHexColorError} from '@/app/domain/errors';
+
 export type convertHexToRgbParams = {
   hex: string;
   alpha?: number;
 };
 
 export function convertHexToRgb(params: convertHexToRgbParams): string {
-  const r = parseInt(params.hex.slice(1, 3), 16),
-    g = parseInt(params.hex.slice(3, 5), 16),
-    b = parseInt(params.hex.slice(5, 7), 16);
+  if (params.hex.length === 7) {
+    const r = parseInt(params.hex.slice(1, 3), 16),
+      g = parseInt(params.hex.slice(3, 5), 16),
+      b = parseInt(params.hex.slice(5, 7), 16);
 
-  if (params.alpha) {
-    return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + params.alpha + ')';
+    if (params.alpha) {
+      return 'rgba(' + r + ', ' + g + ', ' + b + ', ' + params.alpha + ')';
+    } else {
+      return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    }
   } else {
-    return 'rgb(' + r + ', ' + g + ', ' + b + ')';
+    throw new BadHexColorError();
   }
 }
