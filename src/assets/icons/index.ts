4b5fba6af36614e6d2e132cf5be74492c31dad51
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 23:03:30 -0300

    feat: export icons from assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,17 @@
+import DiagnosticIcon from './stethoscope.svg';
+import ConsultationIcon from './patient.svg';
+import NurseIcon from './nurse.svg';
+import AmbulanceIcon from './ambulance.svg';
+import LaboratoryIcon from './flask.svg';
+import MedicineIcon from './medicine.svg';
+import InternetConnectionIcon from './internet_connection.svg';
+
+export {
+  DiagnosticIcon,
+  ConsultationIcon,
+  NurseIcon,
+  AmbulanceIcon,
+  LaboratoryIcon,
+  MedicineIcon,
+  InternetConnectionIcon,
+};
