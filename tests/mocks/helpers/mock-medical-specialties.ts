81333f59d41ead974b11f77d1cf7269861539e77
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 22:38:10 -0300

    test: change image to be url and add min arg to mock

--- tests/mocks/helpers/mock-medical-specialties.ts
+++ tests/mocks/helpers/mock-medical-specialties.ts
@@ -4,15 +4,17 @@ import faker from 'faker';
 export const mockMedicalSpecialty = (): MedicalSpecialties => {
   return {
     name: faker.random.words(1),
-    image: faker.random.words(1),
+    image: faker.internet.url(),
     amountAvailable: faker.datatype.number({min: 0, max: 20}),
     color: faker.random.words(1),
   };
 };
 
-export const mockMedicalSpecialties = (): MedicalSpecialties[] => {
+export const mockMedicalSpecialties = (
+  min: number = 0,
+): MedicalSpecialties[] => {
   const response: MedicalSpecialties[] = [];
-  const randomAmount = faker.datatype.number({min: 0, max: 5});
+  const randomAmount = faker.datatype.number({min, max: 5});
   for (let index = 0; index < randomAmount; index++) {
     response.push(mockMedicalSpecialty());
   }
