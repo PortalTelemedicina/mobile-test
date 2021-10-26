fece90df2a700b8f39751e43001bdbc4cf5f40dd
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 22:34:55 -0300

    chore: remove svg mock from setup and add lib isomorphic fetch

--- jest.setup.js
+++ jest.setup.js
@@ -1,24 +1,14 @@
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 import '@testing-library/jest-native/extend-expect';
-import fetchMock from 'jest-fetch-mock';
-import React from 'react';
-import {View} from 'react-native';
+import 'isomorphic-fetch';
 import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
 import {initialize} from './src/config/locale';
-
-fetchMock.enableMocks();
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
 
 jest.mock('react-native-safe-area-context', () => mockSafeAreaContext);
 
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
-
-jest.doMock('react-native-svg', () => {
-  return {
-    SvgUri: props => <View {...props} />,
-  };
-});
 
 jest.mock('react-native', () => {
   const RN = jest.requireActual('react-native');
