a042bbd01743ce169e6a50c9b6bbcb0ef8150066
Vitor Veras <vitorverasm@gmail.com>
2021-10-26 06:23:37 -0300

    feat: show error message if request fails

--- src/app/UI/pages/list-specialists/list-specialists.tsx
+++ src/app/UI/pages/list-specialists/list-specialists.tsx
@@ -2,8 +2,13 @@ import {MedicalSpecialties, QuickAction} from '@/app/domain/entities';
 import {ListMedicalSpecialties, ListQuickActions} from '@/app/domain/usecases';
 import {ListSpecialistsI18n, SharedI18n, translate} from '@/config/locale';
 import {Layout, Text} from '@ui-kitten/components';
-import React, {useEffect, useState} from 'react';
-import {FlatList, StyleSheet, TouchableOpacity} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {FlatList, StyleSheet, View} from 'react-native';
+import {
+  MedicalSpecialtiesErrorMessage,
+  MedicalSpecialtyCard,
+  QuickActionCard,
+} from './components';
 
 export type ListSpecialistsProps = {
   listMedicalSpecialties: ListMedicalSpecialties;
@@ -14,64 +19,113 @@ const ListSpecialists: React.FC<ListSpecialistsProps> = ({
   listQuickActions,
   listMedicalSpecialties,
 }) => {
-  useEffect(() => {
-    const getQuickActions = () => {
-      const actions = listQuickActions.run();
-      if (actions) {
-        setActions(actions);
-      }
-    };
+  const [actions, setActions] = useState<QuickAction[]>(null);
+  const [medicalSpecialties, setMedicalSpecialties] =
+    useState<MedicalSpecialties[]>(null);
+  const [loadingMedicalSpecialties, setLoadingMedicalSpecialties] =
+    useState<boolean>(false);
+  const [loadingQuickActions, setLoadingQuickActions] =
+    useState<boolean>(false);
+  const [errorMedicalSpecialties, setErrorMedicalSpecialties] =
+    useState<string>(null);
 
-    const getMedicalSpecialties = async () => {
+  const getMedicalSpecialties = useCallback(async () => {
+    try {
+      setLoadingMedicalSpecialties(true);
       const specialties = await listMedicalSpecialties.run();
+      setLoadingMedicalSpecialties(false);
       if (specialties) {
         setMedicalSpecialties(specialties);
+      }
+    } catch (error) {
+      setLoadingMedicalSpecialties(false);
+      setErrorMedicalSpecialties(error.message);
+    }
+  }, [listMedicalSpecialties]);
+
+  useEffect(() => {
+    const getQuickActions = () => {
+      setLoadingQuickActions(true);
+      const actions = listQuickActions.run();
+      setLoadingQuickActions(false);
+      if (actions) {
+        setActions(actions);
       }
     };
 
     getQuickActions();
     getMedicalSpecialties();
     return () => {};
-  }, [listQuickActions, listMedicalSpecialties]);
-
-  const [actions, setActions] = useState<QuickAction[]>(null);
-  const [medicalSpecialties, setMedicalSpecialties] =
-    useState<MedicalSpecialties[]>(null);
+  }, [listQuickActions, getMedicalSpecialties]);
 
   const renderActions = ({item}: {item: QuickAction}) => {
-    const {title, icon, active} = item;
     return (
-      <TouchableOpacity
-        testID={`action-${item.title}`}
-        disabled={!active}
-        onPress={() => {}}
-      >
-        <Text>{title}</Text>
-      </TouchableOpacity>
+      <View style={styles.quickActionWrapper}>
+        <QuickActionCard
+          onPress={() => {}}
+          loading={loadingQuickActions}
+          action={item}
+        />
+      </View>
     );
   };
 
   const renderMedicalSpecialty = ({item}: {item: MedicalSpecialties}) => {
-    const {name} = item;
     return (
-      <TouchableOpacity testID={`medicalspecialty-${name}`} onPress={() => {}}>
-        <Text>{name}</Text>
-      </TouchableOpacity>
+      <View style={styles.medicalSpecialtyWrapper}>
+        <MedicalSpecialtyCard
+          medicalSpecialty={item}
+          onPress={() => {}}
+          loading={loadingMedicalSpecialties}
+        />
+      </View>
     );
   };
 
   return (
     <Layout level="1" style={styles.container}>
-      <Text category="h5">{translate(SharedI18n.specialists)}</Text>
-      <FlatList data={medicalSpecialties} renderItem={renderMedicalSpecialty} />
-      <Text category="h5">{translate(ListSpecialistsI18n.whatDoYouNeed)}</Text>
-      <FlatList data={actions} renderItem={renderActions} />
+      <View style={styles.specialtiesContainer}>
+        <Text category="h5">{translate(SharedI18n.specialists)}</Text>
+        {!errorMedicalSpecialties ? (
+          <FlatList
+            showsHorizontalScrollIndicator={false}
+            data={medicalSpecialties}
+            renderItem={renderMedicalSpecialty}
+            horizontal
+          />
+        ) : (
+          <MedicalSpecialtiesErrorMessage
+            loading={loadingMedicalSpecialties}
+            message={errorMedicalSpecialties}
+            refreshFn={getMedicalSpecialties}
+          />
+        )}
+      </View>
+
+      <View style={styles.whatDoYouNeedContainer}>
+        <Text category="h5" style={styles.whatDoYouNeedText}>
+          {translate(ListSpecialistsI18n.whatDoYouNeed)}
+        </Text>
+        <FlatList
+          style={styles.quickActionFlatlist}
+          numColumns={3}
+          showsVerticalScrollIndicator={false}
+          data={actions}
+          renderItem={renderActions}
+        />
+      </View>
     </Layout>
   );
 };
 
 const styles = StyleSheet.create({
   container: {flex: 1, paddingHorizontal: 20, paddingTop: 30},
+  specialtiesContainer: {flex: 0.8},
+  whatDoYouNeedContainer: {flex: 1.75, marginTop: 10},
+  whatDoYouNeedText: {marginTop: 20},
+  medicalSpecialtyWrapper: {flex: 1, marginTop: 10, marginRight: 15},
+  quickActionWrapper: {flex: 1, margin: 5},
+  quickActionFlatlist: {marginTop: 10},
 });
 
 export default ListSpecialists;
