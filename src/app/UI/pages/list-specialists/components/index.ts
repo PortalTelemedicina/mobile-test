4f09eb0e476ce590be174dda8700685e00476a7b
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 23:49:24 -0300

    feat: add export to page components

---
+++ src/app/UI/pages/list-specialists/components/index.ts
@@ -0,0 +1,4 @@
+export {default as MedicalSpecialtiesErrorMessage} from './medical-specialties-error-message';
+export {default as MedicalSpecialtyCard} from './medical-specialty-card';
+export {default as QuickActionCard} from './quick-action-card';
+export {default as QuickActionIcon} from './quick-action-icon';
