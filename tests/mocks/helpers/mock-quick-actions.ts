9cff4f2421304472bef072a7e36bc9472fe89b66
Vitor Veras <vitorverasm@gmail.com>
2021-10-25 23:40:12 -0300

    test: add mockQuickAction function to mock-quick-actions

--- tests/mocks/helpers/mock-quick-actions.ts
+++ tests/mocks/helpers/mock-quick-actions.ts
@@ -1,15 +1,28 @@
-import {QuickAction} from '@/app/domain/entities';
+import {AvailableIcons, QuickAction} from '@/app/domain/entities';
 import faker from 'faker';
+
+const availableIcons: AvailableIcons[] = [
+  'diagnostic',
+  'consultation',
+  'nurse',
+  'ambulance',
+  'laboratory',
+  'medicine',
+];
+
+export const mockQuickAction = (): QuickAction => {
+  return {
+    title: faker.random.words(1),
+    icon: faker.random.arrayElement(availableIcons),
+    active: faker.datatype.boolean(),
+  };
+};
 
 export const mockQuickActions = (): QuickAction[] => {
   const response: QuickAction[] = [];
   const randomAmount = faker.datatype.number({min: 0, max: 5});
   for (let index = 0; index < randomAmount; index++) {
-    response.push({
-      title: faker.random.words(1),
-      icon: faker.internet.url(),
-      active: faker.datatype.boolean(),
-    });
+    response.push(mockQuickAction());
   }
   return response;
 };
