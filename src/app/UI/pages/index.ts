f8d6275bdf7e0e6815d910b26c357befcf1e0991
Vitor Veras <vitorverasm@gmail.com>
2021-10-27 00:20:12 -0300

    feat: export ListDoctors and ListDoctorsProps

--- src/app/UI/pages/index.ts
+++ src/app/UI/pages/index.ts
@@ -1,6 +1,10 @@
 export {default as Activity} from './activity/activity';
 export {default as Chat} from './chat/chat';
 export {
+  default as ListDoctors,
+  ListDoctorsProps,
+} from './list-doctors/list-doctors';
+export {
   default as ListSpecialists,
   ListSpecialistsProps,
 } from './list-specialists/list-specialists';
