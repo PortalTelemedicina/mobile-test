2036bee9483d9b2b6f3c0c4718cfaead1461cd70
Vitor Veras <vitorverasm@gmail.com>
2021-10-26 23:58:08 -0300

    feat: add urls for each medical specialty

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,4 +1,25 @@
-export const ApiConfig = {
+import {ListDoctorsRoute} from '@/app/domain/protocols/http/mappings';
+
+type ApiConfigSpec = {
+  baseUrl?: string;
+  listDoctorsMapping?: ListDoctorsRoute[];
+};
+
+export const ApiConfig: ApiConfigSpec = {
   baseUrl:
     'https://raw.githubusercontent.com/PortalTelemedicina/mobile-test/main/api',
+  listDoctorsMapping: [
+    {
+      type: 'cardiologist',
+      url: 'https://raw.githubusercontent.com/PortalTelemedicina/mobile-test/main/api/list_specialist_heart.json',
+    },
+    {
+      type: 'dentist',
+      url: 'https://raw.githubusercontent.com/PortalTelemedicina/mobile-test/main/api/list_specialist_dental_care.json',
+    },
+    {
+      type: 'dermatologist',
+      url: 'https://raw.githubusercontent.com/PortalTelemedicina/mobile-test/main/api/list_specialist_dermatology.json',
+    },
+  ],
 };
