11523727cbb48d8ed87baf5f18465c6a484f31e4
Vitor Veras <vitorverasm@gmail.com>
2021-10-26 22:50:56 -0300

    feat: add initial ListDoctors screen

--- src/app/UI/pages/list-doctors/list-doctors.tsx
+++ src/app/UI/pages/list-doctors/list-doctors.tsx
@@ -1,12 +1,23 @@
+import {RouteProp, useRoute} from '@react-navigation/core';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {Text} from '@ui-kitten/components';
 import React from 'react';
 import {View} from 'react-native';
+import {RouteList} from '../../shared/routes';
 
-const ListDoctors = () => {
+export type ListDoctorsProps = {
+  navigation: NativeStackNavigationProp<RouteList, 'LIST-DOCTORS'>;
+};
+
+export type ListDoctorsScreenRouteProp = RouteProp<RouteList, 'LIST-DOCTORS'>;
+
+const ListDoctors: React.FC<ListDoctorsProps> = ({}) => {
+  const route = useRoute<ListDoctorsScreenRouteProp>();
+
   return (
     <View testID="view-list-doctors">
       <Text>ListDoctors</Text>
-      <Text>ListDoctors</Text>
+      <Text>{route.params.type}</Text>
     </View>
   );
 };
